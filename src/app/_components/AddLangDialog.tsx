757c46bdf4580237f471ee2a21fe8aa71ec95101
Luke Gibson <luke.e.gibson@outlook.com>
2024-12-02 22:06:40 +1100

    add functionality to add lang dialog

--- src/app/_components/AddLangDialog.tsx
+++ src/app/_components/AddLangDialog.tsx
@@ -11,10 +11,32 @@ import {
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { cached_getUser, registerLang } from "@/server/queries";
+import { useLocalStorage } from "@rehooks/local-storage";
+import { useRouter } from "next/navigation";
 
 export function AddLangDialog(){
+  const [browserId] = useLocalStorage("fingerprint", "");
+  const [name, setName] = useState("");
+  const [open, setOpen] = useState(false);
+  const [description, setDescription] = useState("");
+  const router = useRouter()
+
+  async function addLang() {
+    if (browserId === "") throw new Error("No browser id");
+    const user = await cached_getUser(browserId);
+    if(!user?.id) throw new Error("User is not registered");
+
+    await registerLang({ name, description }, user.browserId);
+    void router.refresh();
+    void setOpen(false);
+    void setName("")
+    void setDescription("")
+  }
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild><Button>Add Lang</Button></DialogTrigger>
       <DialogContent>
         <DialogHeader>
@@ -27,8 +49,9 @@ export function AddLangDialog(){
               Name
             </Label>
             <Input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               id="name"
-              defaultValue="Lua"
               className="col-span-3"
             />
           </div>
@@ -37,14 +60,15 @@ export function AddLangDialog(){
               Description
             </Label>
             <Input
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
               id="name"
-              defaultValue="A enbedable language most famous for roblox scripting"
               className="col-span-3"
             />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Add language</Button>
+          <Button type="submit" onClick={addLang}>Add language</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
